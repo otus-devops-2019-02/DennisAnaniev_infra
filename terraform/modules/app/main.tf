e76d727c81610b7d4951bb3edb943baa4298e976
Dennis Ananiev <DIAnaniev@gmail.com>
2019-05-30 11:51:34 +0300

    vpc

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -5,32 +5,37 @@ resource "google_compute_instance" "app" {
   tags         = ["reddit-app"]
 
   boot_disk {
-initialize_params { image = "${var.app_disk_image}" }
-}
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
 
   metadata {
     ssh-keys = "dennisananiev:${file(var.public_key_path)}"
   }
 
-network_interface {
-network = "default"
-access_config = {
-nat_ip = "${google_compute_address.app_ip.address}"
-}
-}
-resource "google_compute_firewall" "firewall_puma" {
-name    = "allow-puma-default"
-network = "default"
-allow {
-protocol = "tcp"
-ports    = ["9292"]
-}
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
-}
+  network_interface {
+    network = "default"
 
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+}
+  resource "google_compute_firewall" "firewall_puma" {
+    name    = "allow-puma-default"
+    network = "default"
 
-resource "google_compute_address" "app_ip" {
-name = "reddit-app-ip"
-}
-}
+    allow {
+      protocol = "tcp"
+      ports    = ["9292"]
+    }
+
+    source_ranges = ["0.0.0.0/0"]
+    target_tags   = ["reddit-app"]
+  }
+
+  resource "google_compute_address" "app_ip" {
+    name = "reddit-app-ip"
+  }
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,10 +21,11 @@ variable private_key_path {
 }
 
 variable app_disk_image {
-description = "Disk image for reddit app"
-default = "reddit-app-base"
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
 }
+
 variable db_disk_image {
-description = "Disk image for reddit db"
-default = "reddit-db-base"
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
